fdf657f116c4e50396aaae0d44b4294bf9b96559
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 10:31:56 +0700

    membuat outliner teks

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -3,8 +3,12 @@ package com.example.navigasi.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -51,3 +55,6 @@ fun FormIsian(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OutlinedTextField(
+
+            )
+
