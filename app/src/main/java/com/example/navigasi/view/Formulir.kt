1d3fd8e91feb44540ad3529b5f5fc28521cdb5d3
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 10:32:50 +0700

    Membuat input teks satu baris untuk mengisi nama lengkap dengan lebar 250dp dan jarak atas 20dp

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -55,6 +55,12 @@ fun FormIsian(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OutlinedTextField(
-
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = { Text(text = "Nama lengkap") },
+                onValueChange = {}
             )
 
