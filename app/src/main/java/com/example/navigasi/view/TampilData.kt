d017ff595e101faef98e84ece49e499e2e277d86
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 13:06:57 +0700

    Menampilkan daftar data (items) dalam tampilan kolom dengan jarak antar elemen dan tombol kembali

--- app/src/main/java/com/example/navigasi/view/TampilData.kt
+++ app/src/main/java/com/example/navigasi/view/TampilData.kt
@@ -1,7 +1,14 @@
 package com.example.navigasi.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.R
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,7 +17,12 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
@@ -28,7 +40,25 @@ fun TampilData(
                 title = { Text(text = stringResource(id=R.string.tampil), color = Color.White)},
                 colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.string.teal_700))
             )
-        }){
-
+        }){ isiRuang->
+        Column (modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween){
+            Column(modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.SpacedBy(space = dimensionResource(id = 10.dp))){
+                items.forEach { items ->
+                    Column {
+                        Text(text = item.first.uppercase(), fontSize = 16.sp )
+                        Text(text = item.second, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Cursive, fontSize = 22.sp)
+                    }
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
+                }
+                Spacer(modifier = Modifier.height(height = 10.dp))
+                Button(
+                    modifier = Modifier.fillMaxSize(),
+                    onClick = onBackBtnClick){
+                    Text(text = stringResource(id=R.string.back))
+                }
+            }
+        }
     }
 }
