620a345b6eec4ac9a9bf6077483fed69fa721840
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2025-11-07 12:09:38 +0700

    Parameter lambda untuk menangani aksi tombol "Kembali"

--- app/src/main/java/com/example/navigasi/view/TampilData.kt
+++ app/src/main/java/com/example/navigasi/view/TampilData.kt
@@ -1,2 +1,12 @@
 package com.example.navigasi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onBackBtnClick:()->Unit
+){
+
+}
